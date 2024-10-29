ecece69365770f20b1c8114cc4731367de81de6a
Leidy Viviana Osorio <viviana@movingsuppliesusa.com>
2024-10-29 00:19:07 -0500

    add last files

---
+++ Presentation/Nop.Web/Startup.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Nop.Web.Framework.Infrastructure.Extensions;
+
+namespace Nop.Web
+{
+    /// <summary>
+    /// Represents startup class of application
+    /// </summary>
+    public class Startup
+    {
+        #region Fields
+
+        private readonly IConfiguration _configuration;
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        #endregion
+
+        #region Ctor
+
+        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        {
+            _configuration = configuration;
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Add services to the application and configure service provider
+        /// </summary>
+        /// <param name="services">Collection of service descriptors</param>
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+			// Configure JWT authentication
+			var key = Encoding.ASCII.GetBytes("OPmuTwCXRtVjy5Cx7sQcw1hZumae8Hhl1S1OD3UrIiXDBzS7lvYYSVYF1SQkdWLR");
+
+			services.AddAuthentication(options =>
+			{
+				options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			}).AddJwtBearer(options =>
+			{
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateIssuer = false,
+					ValidateAudience = false,
+					ValidateLifetime = true,
+					ValidateIssuerSigningKey = true,
+					IssuerSigningKey = new SymmetricSecurityKey(key)
+				};
+			});
+
+			// Add Swagger services
+			services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Import Data from Netsuite", Version = "v1" });
+
+                // Configure JWT bearer authentication
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] { }
+                            }
+                        });
+                    });
+
+           // string secretKey = "your_secret_key_here"; // Assuming secret key is stored in configuration under Jwt:Secret
+            //services.AddSingleton(secretKey);
+
+            return services.ConfigureApplicationServices(_configuration, _hostingEnvironment);
+        }
+
+        /// <summary>
+        /// Configure the application HTTP request pipeline
+        /// </summary>
+        /// <param name="application">Builder for configuring an application's request pipeline</param>
+        public void Configure(IApplicationBuilder application)
+        {
+
+			application.UseSwagger();
+			application.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+			});
+			application.UseAuthentication();
+
+			application.ConfigureRequestPipeline();
+        }
+    }
+}
